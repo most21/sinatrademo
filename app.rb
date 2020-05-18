a9e36bbb2b04dca25ee530121f10b451b59fd8bf
most <matthew.ost@grantsolutions.gov>
2020-05-17 21:40:51 -0400

    working version w/ minimal formatting

--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
 
+
 get '/' do
-  "Hello, world"
+  # Read data from file
+  file = File.open("cake.list")
+  data = file.readlines
+  file.close
+
+  # Build an output string in the form of a numbered list
+  # result = ""
+  # for i in 0..data.length-1
+  #   result += "#{i + 1}. #{data[i]}<br>"
+  # end
+
+  for i in 0..data.length-1
+    data[i] = "#{i + 1}. #{data[i]}"
+  end
+  result = data.join("<br>")
+
+
+  # Display output
+  result
 end
