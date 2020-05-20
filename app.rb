b8dceae1a2258a90fe1007165d9941c8506bcff8
most <matthew.ost@grantsolutions.gov>
2020-05-19 20:38:19 -0400

    testing heroku deployment

--- app.rb
+++ app.rb
@@ -2,17 +2,18 @@ require 'sinatra'
 
 
 get '/' do
-  # Read data from file
-  file = File.open("cake.list")
-  data = file.readlines
-  file.close
-
-  # Build an output string in the form of a numbered list
-  for i in 0..data.length-1
-    data[i] = "#{i + 1}. #{data[i]}" # modify each element to be numbered
-  end
-  result = data.join("<br>") # join array together into 1 string
-
-  # Display output
-  result
+  # # Read data from file
+  # file = File.open("cake.list")
+  # data = file.readlines
+  # file.close
+  #
+  # # Build an output string in the form of a numbered list
+  # for i in 0..data.length-1
+  #   data[i] = "#{i + 1}. #{data[i]}" # modify each element to be numbered
+  # end
+  # result = data.join("<br>") # join array together into 1 string
+  #
+  # # Display output
+  # result
+  "Hello, world"
 end
